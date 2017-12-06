6c7f4ce74b75161cc1c54e155e33169a5cece0c8
thalesathayde <thales.athayde.santos@gmail.com>
2017-12-06 17:30:46 -0200

    Verificação da entrada feita

--- prog.py
+++ prog.py
@@ -18,7 +18,7 @@ def info_rede(addr, smask, pref):
     broad=bin_to_dec(broad_bin)
     print("endereço de broadcast: "+broad)
 
-    print("tamanho do prefixo:"+pref)#bota o tamanho do prefixo na tela
+    print("tamanho do prefixo:"+str(pref))#bota o tamanho do prefixo na tela
 
     bottom_addr=min_addr(bin_sub_addr)
     print('primeiro endereço disponivel binario: '+bottom_addr)#primeiro endereço possivel de uso na subrede
@@ -34,7 +34,7 @@ def info_rede(addr, smask, pref):
     print("numero de endereços disponiveis: "+ num_addrs+"\n")
 
 #addr em binario
-def bin_change_addr(num):
+def bin_change_addr(num):#converde endereço decimal pra biinario
     part=[]
     div=num.split('.')
     for i in range(0,4):
@@ -60,7 +60,6 @@ def calc_smask(pref):
 def calc_broadcast(bin_addr,pref):
     substitute=''
     val=int(pref)//8
-    print (val)
     num=0
     for i in range(0,35):
         if bin_addr[i]=='.':
@@ -169,30 +168,61 @@ def bota_ponto(sem_ponto,original):
             resp=resp+sem_ponto[i-ref]
     return resp
 
+def verificaEntrada():
+    args = len(sys.argv)
+    if args == 0:
+        print("Nao existem argumentos passados")
+        return False
+    if sys.argv[1] != "1" and sys.argv[1] != "2" and sys.argv[1] != "3":
+        print("Nao existe esse tipo de modo de funcionamento")
+        return False
+    if args < 3 and sys.argv[1] == "1" or args < 4 and sys.argv[1] != "1":
+        print("Nao tem argumentos suficientes")
+        return False
+    if sys.argv[2].count('.') != 3:
+        print("Quantidade de pontos do endereco esta errado")
+        return False
+    if '.' in sys.argv[3] and sys.argv[3].count('.') != 3:
+        print("Quantidade de pontos do endereco de submascara esta errado")
+        return False
+    vaddr = sys.argv[2].split('.')
+    for j in range(len(vaddr)):
+        if (int)(vaddr[j]) > 255 or (int)(vaddr[j]) < 0:
+            print("Endereco invalido")
+            return False
+    if '.' in sys.argv[3]:
+        vsubmask = sys.argv[3].split('.')
+        for j in range(len(vsubmask)):
+            if (int)(vsubmask[j]) > 255 or (int)(vsubmask[j]) < 0:
+                print("Endereco de submascara invalido")
+                return False
+    return True
 
 
 
 
-tipo =sys.argv[1]
-addr=sys.argv[2]
-submask=' '
-pref=' '
-if '.' in sys.argv[3]:
-    submask=sys.argv[3]
-    pref=addr.count('1')
-else:
-    pref=sys.argv[3]
-    submask=calc_smask(pref)
 
-if tipo == "1":
-    info_rede(addr, submask, pref)
-elif tipo == "2":
-    if '.' in sys.argv[4]:
-        extra_subm=sys.argv[4]
-        extra_pref=extra_subm.count('1')
+if verificaEntrada():
+    tipo =sys.argv[1]
+    addr=sys.argv[2]
+    submask=' '
+    pref=' '
+    if '.' in sys.argv[3]:#se possui um ponto no 3o argumento, entao ele eh uma mascara de rede
+        submask=bin_change_addr(sys.argv[3])
+        pref=submask.count('1')
     else:
-        extra_pref=sys.argv[4]
-        extra_subm=calc_smask(extra_pref)
-    fixed_mode(addr, submask, pref, extra_subm,extra_pref)
-elif tipo == "3":
-    varied_mode()
+        pref=sys.argv[3]
+        submask=calc_smask(pref)
+
+    if tipo == "1":
+        info_rede(addr, submask, pref)
+    elif tipo == "2":
+        if '.' in sys.argv[4]:
+            extra_subm=sys.argv[4]
+            extra_pref=extra_subm.count('1')
+        else:
+            extra_pref=sys.argv[4]
+            extra_subm=calc_smask(extra_pref)
+        fixed_mode(addr, submask, pref, extra_subm,extra_pref)
+    elif tipo == "3":
+        varied_mode()
